1924b4bcb8936b9a8e9a05eb4d2f7a5d6b901154
rhaynes3k <rhaynes3k@gmail.com>
2021-11-04 21:13:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
+let random = Math.floor(Math.random()*10) + 1
+
+let num3 = 44
+let num4 = 8
+let mod = num3 % num4
+
+let max = Math.max(10, 12, 15, 18, 20)
 // Write your code here
